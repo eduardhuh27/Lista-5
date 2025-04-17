43bc11a4140c296f71925736fcc324cecf8e75c5
Eduardo G <eduardogomes5577@gmail.com>
2025-04-17 10:28:49 -0300

    Ex1 corrigido

--- Ex1.c
+++ Ex1.c
@@ -6,14 +6,18 @@ void main()
   printf("Entre com o valor de N:");
   scanf("%d",&n);
   
-  for(i=1;i<=n;i++)
+ /* for(i=1;i<=n;i++)
   {
-    if ((i%2==0) && (i<=n))
+    if ((i%2==0)) //&& (i<=n))
     {
       printf("\n%d",i);
       
     } 
   
+  }*/
+  for(i=0;i<=n;i+=2)
+  {
+    printf("\n%d",i);
   }
 
 }
